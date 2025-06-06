b7b9f4a5d44af39e7914f028c6b103dc783135bb
lyannne <xu.ly@northeastern.edu>
2025-06-06 13:26:30 -0400

    fixed view tests

--- test/view/CalendarTextViewImpTest.java
+++ test/view/CalendarTextViewImpTest.java
@@ -4,22 +4,28 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class CalendarTextViewImpTest {
 
-  CalendarTextViewImp view;
+  private CalendarTextViewImp view;
+  private ByteArrayOutputStream outContent;
 
   @Before
   public void setUp() {
     view = new CalendarTextViewImp();
+    outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
   }
 
   @Test
   public void testPrompt() {
     view.promptForInput();
 
-    assertEquals("Enter your input:", System.out.toString());
+    assertTrue(outContent.toString().contains("Enter your input:"));
   }
 
 }
